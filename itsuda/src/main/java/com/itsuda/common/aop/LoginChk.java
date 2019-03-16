e4e5283cdc69898c32d850b42aebc68075a28f8f
dldrjsdn1 <dldrjsdn1@naver.com>
2019-03-16 15:28:18 +0900

    2019.03.16 / 황준영 / aop 임시수정

--- itsuda/src/main/java/com/itsuda/common/aop/LoginChk.java
+++ itsuda/src/main/java/com/itsuda/common/aop/LoginChk.java
@@ -1,6 +1,14 @@
 package com.itsuda.common.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoginChk {
+	
+	@Before("execution(* com.itsuda.common..*DAOImpl.*(..))")
 	public void loger() {
 		System.out.println("aop테스트");
 	}
